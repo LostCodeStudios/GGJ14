91b4f31cae607667e23f3a34f9812cc721d434dc
Nathaniel Nelson <nelson.nleroy@gmail.com>
2014-01-25 07:23:47 -0700

    Things

---
+++ GlobalGameJam/GlobalGameJam/GlobalGameJam/Entities/Systems/DirectionalSpriteSystem.cs
@@ -0,0 +1,28 @@
+﻿using GameLibrary.Dependencies.Entities;
+using GameLibrary.Entities.Components.Physics;
+using GlobalGameJam.Entities.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GlobalGameJam.Entities.Systems
+{
+    class DirectionalSpriteSystem : EntityProcessingSystem
+    {
+        public DirectionalSpriteSystem()
+            : base(typeof(DirectionalSprite))
+        {
+
+        }
+
+        public override void Process(Entity e)
+        {
+            DirectionalSprite ds = e.GetComponent<DirectionalSprite>();
+
+            Body b = e.GetComponent<Body>();
+
+
+        }
+    }
+}
