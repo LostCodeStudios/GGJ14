0573cc405abc8d9f494db24c7982a9f202f76cb1
MadcowDeity <carlostnb@gmail.com>
2014-01-24 21:27:05 -0700

    Adding regions to the world.

--- GlobalGameJam/GlobalGameJam/GlobalGameJam/BoblinWorld.cs
+++ GlobalGameJam/GlobalGameJam/GlobalGameJam/BoblinWorld.cs
@@ -8,5 +8,53 @@ namespace GlobalGameJam
 {
     class BoblinWorld : World
     {
+        #region Constructors
+
+        #endregion
+
+        #region Initialization
+
+        /// <summary>
+        /// Builds the entitties and the world.
+        /// </summary>
+        /// <param name="Content"></param>
+        /// <param name="args"></param>
+        protected override void BuildEntities(Microsoft.Xna.Framework.Content.ContentManager Content, params object[] args)
+        {
+            base.BuildEntities(Content, args);
+        }
+
+        /// <summary>
+        /// Builds the templates for entitiy creation.
+        /// </summary>
+        /// <param name="Content"></param>
+        /// <param name="args"></param>
+        protected override void BuildTemplates(Microsoft.Xna.Framework.Content.ContentManager Content, params object[] args)
+        {
+            base.BuildTemplates(Content, args);
+        }
+
+        /// <summary>
+        /// Builds the systems
+        /// </summary>
+        protected override void BuildSystems()
+        {
+            base.BuildSystems();
+        }
+
+        #endregion
+
+        #region Functioning Loop
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Fields
+
+
+        #endregion
     }
 }
