8b7edec92c58102c363d0cc0dc7597eebf3aaa3d
MadcowDeity <carlostnb@gmail.com>
2014-01-25 17:41:47 -0700

    zfsdf

--- GlobalGameJam/GlobalGameJam/GlobalGameJam/Entities/Systems/PlayerControlSystem.cs
+++ GlobalGameJam/GlobalGameJam/GlobalGameJam/Entities/Systems/PlayerControlSystem.cs
@@ -70,7 +70,8 @@ namespace GlobalGameJam.Entities.Systems
                 Vector2 mouseWorldLoc = mouseLoc - ScreenHelper.Center;
                 Vector2 aiming = b.Position - camera.ConvertScreenToWorld(mouseLoc);
                 aiming.Normalize();
-                b.LinearVelocity = -aiming * PLAYER_SPEED * 1.5f;
+                b.LinearVelocity = -aiming * PLAYER_SPEED * 1.5f; //dude please no "shit nigga" find the off switch please 
+                //for today and for the future where it afctually is an important skill 
 
                 e.GetComponent<Health>().MaxHealth = 10;
                 Sprite s = e.GetComponent<Sprite>();
